dd3b974af88dac52744528e254b43df8e3e9d794
ATSISTEMAS\msanchez.humanes <msanchez.humanes@atsistemas.com>
2023-06-01 10:53:24 +0200

    url cambiada

--- src/helpers/Global.jsx
+++ src/helpers/Global.jsx
@@ -1,5 +1,5 @@
 export const Global = {
   // url: "http://localhost:3900/api/",
-  // url: "https://servidor-read-social-react-render.onrender.com/api/",
-  url: "https://redsocialbackend.netlify.app/api/",
+  url: "https://servidor-read-social-react-render.onrender.com/api/",
+  // url: "https://redsocialbackend.netlify.app/api/",
 };

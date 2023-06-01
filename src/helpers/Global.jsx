dc579a600d63b291357efc3dff16ee76c5e95edf
ATSISTEMAS\msanchez.humanes <msanchez.humanes@atsistemas.com>
2023-06-01 10:47:58 +0200

    Servidor cambiado a netlify

--- src/helpers/Global.jsx
+++ src/helpers/Global.jsx
@@ -1,4 +1,5 @@
 export const Global = {
   // url: "http://localhost:3900/api/",
-  url: "https://servidor-read-social-react-render.onrender.com/api/",
+  // url: "https://servidor-read-social-react-render.onrender.com/api/",
+  url: "redsocialbackend.netlify.app/api/",
 };
